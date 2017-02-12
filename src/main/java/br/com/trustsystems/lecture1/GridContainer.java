f47d59dbb87518664308ddcace2b6c5224f560b0
Wenderson Ferreira de Souza <wenderson@trustsystems.com.br>
2017-02-12 00:00:38 -0200

    updating project with new specifications, GridContainer

---
+++ src/main/java/br/com/trustsystems/lecture1/GridContainer.java
@@ -0,0 +1,53 @@
+package br.com.trustsystems.lecture1;
+
+import java.util.Arrays;
+
+public class GridContainer extends Container {
+	private Component[][] elementsArray;
+	private int lineCounter;
+	private int columnCounter;
+	private int width;
+	private int height;
+
+	public GridContainer(int width, int height) {
+		this.width = width;
+		this.height = height;
+		elementsArray = new Component[width][height];
+	}
+
+	@Override
+	public void addComponent(Component c) {
+		if (lineCounter == height && columnCounter == width) {
+			System.out.println("It's not possible to add new elements");
+		} else {
+			elementsArray[lineCounter][columnCounter] = c;
+			columnCounter++;
+			if (columnCounter == width) {
+				lineCounter++;
+				if (lineCounter < height) {
+					columnCounter = 0;
+				}
+			}
+		}
+	}
+
+	@Override
+	public void removeComponent(Component c) {
+		for (int i = 0; i < elementsArray.length; i++) {
+			for (int j = 0; j < elementsArray[i].length; j++) {
+				if (elementsArray[i][j] == c) {
+					elementsArray[i][j] = null;
+				}
+			}
+		}
+	}
+
+	@Override
+	public void doLayout() {
+		System.out.println("The Container being used is the GridContainer");
+		System.out.println("That's elements presented in this Container");
+		System.out.println(Arrays.deepToString(elementsArray));
+		System.out.println("Using of inheritance to close Container");
+		dispose();
+	}
+}
