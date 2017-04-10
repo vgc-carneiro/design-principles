d712aee515c72153de80dccfd541dd342589bfd6
vgc-carneiro <vgc.carneiro@gmail.com>
2017-04-10 08:12:00 -0300

    Add files via upload
    Adicionar novas classes responsáveis pela criação de containers com borda.

---
+++ FlowContainerSolidBorder.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.trustsystems.lecture1;
+
+/**
+ *
+ * @author administrador
+ */
+public class FlowContainerSolidBorder extends FlowContainer {
+    
+    
+    @Override
+    public void doLayout(){
+        System.out.println("The Container being used is the FlowContainerSolidBorder");
+        System.out.println("That's elements presented in this Container");
+        System.out.println(super.getClass());
+        System.out.println("Now, the container has a solid border");
+        System.out.println("Using of inheritance to close container");
+        dispose();
+    }
+    
+}
---
+++ GridContainerSolidBorder.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.trustsystems.lecture1;
+
+import java.util.Arrays;
+
+/**
+ *
+ * @author administrador
+ */
+public class GridContainerSolidBorder extends GridContainer {
+    
+    public GridContainerSolidBorder(int width, int height) {
+        super(width, height);
+    }
+    
+    @Override
+    public void doLayout() {
+		System.out.println("The Container being used is the GridContainer");
+		System.out.println("That's elements presented in this Container");
+		System.out.println(Arrays.deepToString(super.getElementsArray()));
+                System.out.println("Now, the container has a solid border");
+		System.out.println("Using of inheritance to close Container");
+		dispose();
+	}
+    
+}
